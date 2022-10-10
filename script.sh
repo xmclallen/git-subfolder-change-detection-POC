90ab581daec6163979b9521195e00d232577f741
xavier.mclallen <xavier.mclallen@wgu.edu>
2022-10-10 10:04:42 -0700

    change

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 PREVIOUS_HEAD=`git rev-parse --short HEAD@{1} -1`
 
 # Now use that prior HEAD to determine what files have been changed
-FILE_LIST=`git diff --name-only $COMMIT_ID`
+FILE_LIST=`git diff --name-only $PREVIOUS_HEAD`
 
 # Simplify that list to just the folders
 AFFECTED_FOLDERS= `echo "$FILE_LIST" | awk -F'/' 'NF!=1{print $1}' | sort -u`
