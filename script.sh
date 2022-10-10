d3c112940e9fb169a5c0b07de6a8deb28535422a
xavier.mclallen <xavier.mclallen@wgu.edu>
2022-10-10 10:05:28 -0700

    change

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 # HEAD@{1} -1 should give us the the HEAD that was before the most recent action
 # In this case, that should be before we merged / did a PR
 PREVIOUS_HEAD=`git rev-parse --short HEAD@{1} -1`
+echo $PREVIOUS_HEAD
 
 # Now use that prior HEAD to determine what files have been changed
 FILE_LIST=`git diff --name-only $PREVIOUS_HEAD`
